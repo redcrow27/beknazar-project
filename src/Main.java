69acc8fe792ad48b8308d9c11d9d1155b3eaba30
Beknazar Suranchiyev <Suranchiyev96@gmail.com>
2020-08-02 09:37:40 -0400

    Studnet & more prints

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
 	public static void main(String[] args) {
 		for(int i = 0; i < 10; i++) {
 			System.out.println("Hello, World!");
+			System.out.println("Hello, World!");
+			System.out.println("Hello, World!");
 		}
 	}
 }
